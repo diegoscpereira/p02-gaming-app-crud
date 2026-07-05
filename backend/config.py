530ee0f311328dded7736e2d3dd3e6ac537a5a9b
Diego <dpereira96@gmail.com>
2026-07-05 19:59:36 +0100

    feat: adding models.py

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     rawg_api_key: str
-    database_url: str = 'sqlite:///./library.db'
+    database_url: str = "sqlite:///./library.db"
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from backend.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
+
+class Library(Base):
+    __tablename__ = "library"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    slug: Mapped[str]
+    name: Mapped[str]
+    game_description: Mapped[str]
+    released: Mapped[date]
+    background_image: Mapped[str] | None
+    metacritic: Mapped[int] | None
+    rating: Mapped[float] | None
+    ratings_count: Mapped[int] | None
+    updated: Mapped[date]
+    platforms: Mapped[str]
+    genres: Mapped[str]
+    developers: Mapped[str]
+    status: Mapped[str] | None
+    my_rating: Mapped[int] | None
+    comment: Mapped[str] | None
+    added_at: Mapped[date] = mapped_column(default=date.today)
